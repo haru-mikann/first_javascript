6ea34e41930d5bc334cbbdc3f6fab7e0fca8cf17
haruto_yamazaki <itigo8610@gmail.com>
2022-10-23 14:13:42 +0900

    メイン機能

--- first.js
+++ first.js
@@ -6,44 +6,62 @@ let player_damage = 0
 
 let flag = false
 let runFlag = false
-const mainlog = document.getElementById("singolog")
-const htmlTitle = document.getElementById("title")
 
-document.write("しんごHP" + "[#####]")
-document.write("モンスターHP" + "[##########]")
-htmlTitle.textContent = "モンスターが現れた"
+
+document.getElementById("title").textContent = "モンスターが現れた"
+
+
+const singoHP = document.getElementById("singoHP")
+const monsterHP = document.getElementById("monsterHP")
+
+
+
+
+const singolog = document.getElementById("singolog")
+const monsterlog = document.getElementById("monsterlog")
+const syslog = document.getElementById("syslog")
 
 function attackButton(){
+    const getHpText = (hp) => {
+        if(hp>0){
+            const num =  Math.floor(hp / 20)
+            return '#'.repeat(num)
+        }else{
+            return "-"
+        }
+    }
     if(player_life > 0 && monster_life > 0 && runFlag == false){
         monster_damage = parseInt(Math.floor(Math.random() * 30))
         monster_life = monster_life - monster_damage
-        mainlog.textContent = "モンスターは" + monster_damage + "のダメージを受けた"
+        monsterlog.textContent = "モンスターは" + monster_damage + "のダメージを受けた"
+        monsterHP.textContent = "モンスターHP:" + getHpText(monster_life)
     }
-
+    
     if(monster_life <= 0 && flag == false){
-        mainlog.textContent = "モンスターは力尽きた"
+        monsterlog.textContent = "モンスターは力尽きた"
         flag = true
     }
-
+    
     if(player_life > 0 && monster_life > 0 && runFlag == false){
         player_damage = parseInt(Math.floor(Math.random() * 30))
         player_life = player_life - player_damage
-        mainlog.textContent = "しんごは" + player_damage + "のダメージを受けた"
+        singolog.textContent = "しんごは" + player_damage + "のダメージを受けた"
+        singoHP.textContent ="しんごHP:" + getHpText(player_life)
     }
-
+    
     if(player_life <= 0 && flag == false){
-        mainlog.textContent = "しんごは力尽きた"
+        singolog.textContent = "しんごは力尽きた"
         flag = true
     }
 }
 
 function runawayButton(){
     if(flag == false && runFlag == false){
-        mainlog.textContent = "逃げ切れた"
+        syslog.textContent = "逃げ切れた"
         runFlag = true    
     }
 }
 
+function item(){
 
-
-
+}
