b8dd7787bdc57fb530a0c6255dab77c55d97b721
haruto_yamazaki <itigo8610@gmail.com>
2022-10-27 16:37:52 +0900

    class整理

--- log.js
+++ log.js
@@ -1,9 +1,12 @@
 let logLists = []
 let outlog = []
 const log = (addlogComent) => {
-    logLists[logLists.length] = addlogComent
+    logLists = [...logLists ,addlogComent]
 }
 const mainlog = (logLists) => {
         outlog =String(logLists).replace(/,/g,"\n")
+        // for(let j = 0; logLists == j; j++){
+        //     document.getElementById("syslog").appendChild("div") = logLists[j]
+        // }
     return outlog
 } 
